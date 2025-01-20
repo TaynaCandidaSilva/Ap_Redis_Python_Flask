55c8621eb2846aa632978dd6b50bffcfe2d85289
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-20 10:30:31 -0300

    Conhecendo os principais comandos do Redis

--- redis_raw.py
+++ redis_raw.py
@@ -12,5 +12,26 @@ meu_valor = redis_conn.get("chave_1").decode("utf-8")
 # Para Delete no banco
 redis_conn.delete("chave_1")
 
-print(meu_valor)
-print(type(meu_valor))
+# Comandos para hash
+
+meu_hash = {  # chave de exemplo
+    "nome": "joao",  # field: values
+    "idade": 30,
+    "cidade": "sao paulo",
+}
+# O HSET serve para Insert e Update
+
+redis_conn.hset("meu_hash", "nome", "joao")
+redis_conn.hset("meu_hash", "idade", 30)
+redis_conn.hset("meu_hash", "cidade", "sao paulo")
+
+valor1 = redis_conn.hget("meu_hash", "nome").decode("utf-8")
+
+redis_conn.hdel("meu_hash", "cidade")
+
+## Busca por existencia
+elem = redis_conn.exists("chave_1")
+print(elem)
+
+elem2 = redis_conn.hexists("meu_hash", "nome")
+print(elem2)
