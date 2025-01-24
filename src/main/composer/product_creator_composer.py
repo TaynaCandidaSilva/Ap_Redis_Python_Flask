587a05c9aaa017bf107a2f3de42a4a831e36ba3a
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-24 16:22:39 -0300

    Implemantacao: add composer ao projeto

---
+++ src/main/composer/product_creator_composer.py
@@ -0,0 +1,17 @@
+from src.main.server.server_setting import (
+    redis_connection_handler,
+    sqlite_connection_handler,
+)
+from src.models.redis.repository.redis_repository import RedisRepository
+from src.models.sqlite.repository.products_repository import ProductsRepository
+from src.data.product_creator import ProductCreate
+
+
+def product_creator_composer():
+    redis_conn = redis_connection_handler.get_connection()
+    sqlite_conn = sqlite_connection_handler.get_connection()
+
+    redis_repo = RedisRepository(redis_conn)
+    products_repo = ProductsRepository(sqlite_conn)
+
+    return ProductCreate(redis_repo, products_repo)
